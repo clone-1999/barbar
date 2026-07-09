3f1404e7f699f0487b084b7f47885ab935b90ba1
clone-1999 <hantharlay6578@gmail.com>
2026-07-09 16:34:13 +0630

    Create init.py

---
+++ games/init.py
@@ -0,0 +1,7 @@
+from .uno_game import UnoGame
+from .werewolf_game import WerewolfGame
+from .quiz_game import QuizGame
+from .rps_game import RPSGame
+from .hangman_game import HangmanGame
+
+__all__ = ["UnoGame", "WerewolfGame", "QuizGame", "RPSGame", "HangmanGame"]
